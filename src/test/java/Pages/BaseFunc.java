087d10d2308dc513001f50650bb0aaf3a7344361
Evita <stoukly@inbox.lv>
2023-05-31 21:07:03 +0300

    Added Test on page for Tickets test

---
+++ src/test/java/Pages/BaseFunc.java
@@ -0,0 +1,7 @@
+package Pages;
+
+public class BaseFunc {
+    public BaseFunc() {
+        System.out.println("Hi I'm constructor! Welcome to this World!");
+    }
+}
---
+++ src/test/java/TicketsTestOnPage.java
@@ -0,0 +1,10 @@
+import Pages.BaseFunc;
+import org.junit.jupiter.api.Test;
+
+public class TicketsTestOnPage {
+    @Test
+    public void successTicketsBookCheck() {
+        BaseFunc baseFunc = new BaseFunc();
+
+    }
+}
